d80d162e46fe954703b8d3e5d53a85cdca3427a3
Nikola Filipovski <nfilipovski@yahoo.com>
2021-04-03 03:27:17 +0200

    03.04.2021

---
+++ Covid19WebApp/Covid19.Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19.Data
+{
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
